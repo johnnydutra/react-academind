ab9401b8cb19e5b4d89f6616055a1f8ec4ae65af
Joao Dutra <jrpu@gft.com>
2022-09-08 15:53:28 -0300

    adds capability to add new expense

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
+import { useState } from "react";
+
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const DUMMY_EXPENSES = [
+  { id: "e1", title: "Olympikus Corre Vento", amount: 299.99, date: new Date(2022, 7, 2)},
+  { id: "e2", title: "New Balance RC Elite V2", amount: 1399.99, date: new Date(2022, 6, 19)},
+  { id: "e3", title: "Fila Kenya Racer 5", amount: 399.99, date: new Date(2022, 3, 23)},
+  { id: "e4", title: "Saucony Fastwitch 9", amount: 349.99, date: new Date(2021, 11, 11)}
+];
+
 const App = () => {
-  const expenses = [
-    { id: "e1", title: "Olympikus Corre Vento", amount: 299.99, date: new Date(2022, 7, 2)},
-    { id: "e2", title: "New Balance RC Elite V2", amount: 1399.99, date: new Date(2022, 6, 19)},
-    { id: "e3", title: "Fila Kenya Racer 5", amount: 399.99, date: new Date(2022, 3, 23)},
-    { id: "e4", title: "Saucony Fastwitch 9", amount: 349.99, date: new Date(2021, 11, 11)}
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    console.log(expense);
+    setExpenses(previousExpenses =>{
+      return [expense, ...previousExpenses];
+    });
   };
 
   return (
