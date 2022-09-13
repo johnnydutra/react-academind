e820aa63ff12d26e5515e8ef584f171f7f6597a1
Johnny Dutra <johnny.dutra@gmail.com>
2022-09-13 11:07:05 -0300

    adds ExpensesChart functionality

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -3,7 +3,7 @@ import "./ChartBar.css";
 const ChartBar = (props) => {
   let barFill = "0%";
 
-  if (props.max > 0) {
+  if (props.maxValue > 0) {
     barFill = Math.round((props.value / props.maxValue) * 100) + "%";
   }
 
@@ -12,11 +12,11 @@ const ChartBar = (props) => {
       <div className="chart-bar__inner">
         <div 
           className="chart-bar__fill" 
-          style={{height: barFill}}></div>
+          style={{ height: barFill }}></div>
       </div>
       <div className="chart-bar__label">{props.label}</div>
     </div>
-  )
+  );
 };
 
 export default ChartBar;
