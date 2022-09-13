22c8558a5ba0129a400a425bfad44bc9b26dbf25
Johnny Dutra <johnny.dutra@gmail.com>
2022-09-13 08:43:49 -0300

    adds ExpenseForm toggling capability

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,18 +1,32 @@
+import { useState } from "react";
+
 import ExpenseForm from "./ExpenseForm"
 import "./NewExpense.css"
 
 const NewExpense = (props) => {
+  const [isFormShowing, setIsFormShowing] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString()
     };
     props.onAddExpense(expenseData);
+    setIsFormShowing(false);
+  };
+
+  const showFormHandler = () => {
+    setIsFormShowing(true);
+  };
+
+  const closeFormHandler = () => {
+    setIsFormShowing(false);
   };
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!isEditing && <button onClick={showFormHandler}>Add New Expense</button>}
+      {isEditing && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={closeFormHandler} />}
     </div>
   );
 };
