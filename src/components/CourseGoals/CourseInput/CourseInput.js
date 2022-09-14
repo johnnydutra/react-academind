3886c97dd671c129f25c5266a4462336170c51b1
Johnny Dutra <johnny.dutra@gmail.com>
2022-09-14 13:54:22 -0300

    adds input field style change and reset capability

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,21 +5,39 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValidInput, setIsValidInput] = useState(true);
 
   const goalInputChangeHandler = event => {
+    if (event.target.value.trim().length > 0) {
+      setIsValidInput(true);
+    }
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsValidInput(false);
+      return;
+    }
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <label
+          style={{ 
+            color: !isValidInput ? "red" : "black" 
+          }}>Course Goal</label>
+        <input
+          style={{
+            borderColor: !isValidInput ? "red" : "black", 
+            background: !isValidInput ? "salmon" : "transparent"
+          }} 
+          type="text" 
+          onChange={goalInputChangeHandler} 
+        />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
