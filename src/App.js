5c205896d9e560a5f978a144af0848b53f521a1a
Johnny Dutra <johnny.dutra@gmail.com>
2022-09-24 14:46:59 -0300

    initial commit for user-data project

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
-import { useState } from "react";
+import React from 'react';
 
-import Expenses from "./components/Expenses/Expenses";
-import NewExpense from "./components/NewExpense/NewExpense";
 
-const DUMMY_EXPENSES = [
-  { id: "e1", title: "Olympikus Corre Vento", amount: 299.99, date: new Date(2022, 7, 2)},
-  { id: "e2", title: "New Balance RC Elite V2", amount: 1399.99, date: new Date(2022, 6, 19)},
-  { id: "e3", title: "Fila Kenya Racer 5", amount: 399.99, date: new Date(2022, 3, 23)},
-  { id: "e4", title: "Saucony Fastwitch 9", amount: 349.99, date: new Date(2021, 11, 11)}
-];
-
-const App = () => {
-  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
-
-  const addExpenseHandler = (expense) => {
-    setExpenses(previousExpenses =>{
-      return [expense, ...previousExpenses];
-    });
-  };
-
+function App() {
   return (
     <div>
-      <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses items={expenses} />
+
     </div>
   );
 }
