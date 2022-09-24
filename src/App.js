56b580f6873dbb9d8092d3a61860b601ecf1227a
Johnny Dutra <johnny.dutra@gmail.com>
2022-09-24 15:54:46 -0300

    adds user storing capability

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import AddUser from "./components/Users/AddUser";
 import UsersList from './components/Users/UsersList';
 
-
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (username, age) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList, 
+        {
+          name: username, 
+          age: age, 
+          id: Math.random().toString()
+        }
+      ];
+    });
+  };
+
   return (
     <div>
-      <AddUser />
-      <UsersList users={[]} />
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
     </div>
   );
 }
